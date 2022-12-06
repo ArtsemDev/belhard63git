b3f2c7b63b8f258a15d53bc5264ad2d78025e2d7
milvus <thommy.python.dev@gmail.com>
2022-12-06 19:30:42 +0300

    if-for

---
+++ 5.py
@@ -0,0 +1,63 @@
+# n = int(input('n: '))
+# data = {i: {'name': input('name: '), 'email': input('email: ')} for i in range(n)}
+# print(data)
+
+# text = input()
+# data = {i: text.count(i) for i in text}
+
+# a = 3
+# if a % 2:
+#     print('odd')
+#     print('odd')
+# elif a == 0:
+#     print('zero')
+#     print('zero')
+# else:
+#     print('even')
+#     print('even')
+
+# a = int(input())
+# # if a % 2:
+# #     is_even = 'odd'
+# # else:
+# #     is_even = 'even'
+#
+# # is_even = 'odd' if a % 2 else 'even'
+# print('odd' if a % 2 else 'even')
+
+# x = True
+# y = False
+# z = False
+# if not x or y:
+#     print(1)
+# elif not x or not y and z:
+#     print(2)
+# elif not x or y or not y and x:
+#     print(3)
+# else:
+#     print(4)
+# for number in range(1, 10, 2):
+#     number = number ** 2
+#     print(number, end=' ')
+#
+
+# words = ['hello', 'python', 'world']
+# for word in words:
+#     print(word)
+# for i, elem in enumerate(words):
+#     print(i, elem)
+# for i in range(len(words)):
+#     print(i)
+
+# data = {
+#     'name': 'vasya',
+#     'email': 'vasya@info.com',
+#     'age': 23
+# }
+
+# for key in data:
+#     print(key)
+# for val in data.values():
+#     print(val)
+# for key, val in data.items():
+#     print(key, val)
