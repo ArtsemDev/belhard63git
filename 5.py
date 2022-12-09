74f4e5fdc4bdfb1e2c8d672a0696d112096875ca
milvus <thommy.python.dev@gmail.com>
2022-12-09 18:51:05 +0300

    tasks

--- 5.py
+++ 5.py
@@ -139,10 +139,60 @@
 # for i in number:
 #     s += int(i)
 # print(f'{s=}')
-numbers = []
-for i in range(1, 101):
-    if (i ** 2) % 5:
-        numbers.append(i**2)
-print(numbers)
+# numbers = []
+# for i in range(1, 101):
+#     if (i ** 2) % 5:
+#         numbers.append(i**2)
+# print(numbers)
+#
+# numbers = [i**2 for i in range(1, 101) if (i ** 2) % 5]
 
-numbers = [i**2 for i in range(1, 101) if (i ** 2) % 5]
+# N = int(input('N: '))
+# M = int(input('M: '))
+# K = int(input('K: '))
+# start = (K // M) * M + M
+# stop = start + M * N
+# for i in range(start, stop, M):
+#     print(i)
+# numbers = []
+# while len(numbers) < N:
+#     if not K % M:
+#         numbers.append(K)
+#         K += M
+#     else:
+#         K += 1
+# print(numbers)
+
+# N = int(input('N: '))
+# M = int(input('M: '))
+# for i in range(2, N+1, 2):
+#     print(i, end=' ')
+#     if not i % 10:
+#         print()
+
+# c = 0
+# for i in range(2, N+1, 2):
+#     c += 1
+#     if c > M:
+#         c = 1
+#         print()
+#     print(i, end=' ')
+
+# 2 4 6 8 10
+# 12 14 16 18 20
+# 22 24 26 28 30
+# 32 34 36 38 40
+# for i in range(2, N + 1, 10):
+#     for j in range(i, i+9, 2):
+#         if j <= N:
+#             print(j, end=' ')
+#         else:
+#             break
+#     print()
+
+# from itertools import zip_longest
+# numbers = [i for i in range(2, N+1, 2)]
+# numbers_iter = iter(numbers)
+# numbers = list(zip_longest(*([numbers_iter]*5)))
+# for line in numbers:
+#     print(line)
