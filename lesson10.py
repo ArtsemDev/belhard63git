99aa3552db8e8246a1bba4c3ae2fc1502c7e994a
milvus <thommy.python.dev@gmail.com>
2022-12-27 20:33:18 +0300

    files

---
+++ lesson10.py
@@ -0,0 +1,67 @@
+# # # # file = open('input.txt', 'r', encoding='utf-8')
+# # # # text = [line.strip() for line in file if line.strip()]
+# # # # file.seek(0)
+# # # # print(file.readline())
+# # # # file.close()
+# # # with open('input.txt', 'r', encoding='utf-8') as file:
+# # #     lines: list[str] = file.readlines()
+# # #
+# # # lines[3] = '3. Encoding\n'
+# # # with open('input.txt', 'w', encoding='utf-8') as file:
+# # #     file.writelines(lines)
+# #
+# # from json import load, loads, dump, dumps
+# #
+# # # with open('user.json', 'r', encoding='utf-8') as file:
+# # #     data = load(file)
+# # # print(data)
+# # # text = '{"name": "Alex", "age": 32}'
+# # # data2 = loads(text)
+# # # print(data2)
+# #
+# # data = {
+# #     'name': 'Доминик',
+# #     'city': 'LA',
+# #     'is_human': True,
+# #     'car': None
+# # }
+# # # text = dumps(data, indent=2, ensure_ascii=False)
+# # # print(text)
+# # with open('dominik.json', 'w', encoding='utf-8') as file:
+# #     dump(data, file, indent=2, ensure_ascii=False)
+#
+# from csv import reader, DictReader, writer, DictWriter
+#
+# with open('users.csv', 'r', encoding='utf-8') as file:
+#     data = list(DictReader(file))
+# #     fieldnames = file.readline().strip().split(',')
+# #     data = [
+# #         dict(zip(fieldnames, line.strip().split(',')))
+# #         for line in file
+# #     ]
+# #     # data = list(reader(file))
+# #     # data = [line.strip().split(',') for line in file]
+# #
+# # print(data)
+# # data = [
+# #     ['name', 'age'],
+# #     ['alex', '34'],
+# #     ['pavel', '43']
+# # ]
+# data = [
+#     {'name': 'alex', 'age': '34'},
+#     {'name': 'pavel', 'age': '43'}
+# ]
+# with open('new_user.csv', 'w', encoding='utf-8') as file:
+#     keys = list(data[0].keys())
+#     wrt = DictWriter(file, keys)
+#     wrt.writeheader()
+#     wrt.writerows(data)
+#     # wrt = writer(file)
+#     # wrt.writerows(data)
+#     # text = ''
+#     # for line in data:
+#     #     line = ','.join(line)
+#     #     line += '\n'
+#     #     text += line
+#     # file.write(text)
